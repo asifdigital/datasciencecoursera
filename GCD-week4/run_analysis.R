1799ab3e6ca586a5d1bb372d0cfc14f9a0c608ff
Asif Digital <asifha1976@gmail.com>
2019-07-21 18:31:30 +0500

    Coursera: Getting and Cleaning Data, Week4 Assignemnt

---
+++ GCD-week4/run_analysis.R
@@ -0,0 +1,51 @@
+#  title: "run_analysis.R"
+#author: "Asif Hameed"
+#date: "7/21/2019"
+#Course: Getting and Cleaning data, Week4 Assignment
+
+library(dplyr) 
+
+#load features & load activity lables
+activityLabels<-read.table("UCI HAR Dataset/activity_labels.txt")
+features<-read.table("UCI HAR Dataset/features.txt")
+
+#make the column names valid
+features[,2]<-gsub("[()-]",".",features[,2])
+
+#load test data 
+xTest<-read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE, col.names=features[,2])
+yTest<-read.csv("UCI HAR Dataset/test/y_test.txt", sep="", header=FALSE)
+testSubject<-read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+
+#load train data
+xTrain<-read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE, col.names=features[,2])
+yTrain<-read.csv("UCI HAR Dataset/train/y_train.txt", sep="", header=FALSE)
+trainSubject<-read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
+
+
+
+# Step1 Merges the training and the test sets to create one data set.
+X<-rbind(xTest, xTrain)
+Y<-rbind(yTest,yTrain)
+subjects<-rbind(testSubject,trainSubject)
+
+
+# Step2 Extracts only the measurements on the mean and standard deviation for each measurement.
+selectedFeatures<-grep("*mean*|std*",features[,2], value=TRUE)
+X<-select(X,selectedFeatures)
+
+#Step3 Uses descriptive activity names to name the activities in the data set 
+colnames(Y)<-"ActivityID"
+colnames(subjects)<-"Subjects"
+colnames(activityLabels)<-c("ActivityID","Activity")
+
+#Step4 Appropriately labels the data set with descriptive variable names.
+X2<-cbind(X,Y,subjects)
+X2<-merge(X2,activityLabels,by.x="ActivityID",by.y="ActivityID")
+
+#Step5 From the data set in step 4, creates a second, independent tidy data set with the average
+#  of each variable for each activity and each subject
+library(reshape2)
+X3<-melt(X2,id=c("Activity","Subjects"),measure.vars = selectedFeatures)
+X4<-dcast(X3, Activity ~ variable, mean)
+X5<-dcast(X3, Subjects ~ variable, mean)
